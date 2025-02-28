d8dd03193b97ef89e27800acd9ab754e9bfa14c6
lkas96 <kwek_lawson@hotmail.com>
2025-03-01 00:13:24 +0800

    Initial + 3 endpoints data scrape

---
+++ src/main/java/com/lask/poopal_server/poopal_server/PoopalServerApplication.java
@@ -0,0 +1,13 @@
+package com.lask.poopal_server.poopal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoopalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoopalServerApplication.class, args);
+	}
+
+}
