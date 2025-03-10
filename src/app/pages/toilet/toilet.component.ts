fce8edebfa216103df48e06b7dbb8c8e5587b221
lkas96 <kwek_lawson@hotmail.com>
2025-03-10 20:39:42 +0800

    placeholders

---
+++ src/app/pages/toilet/toilet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toilet',
+  standalone: false,
+  templateUrl: './toilet.component.html',
+  styleUrl: './toilet.component.css'
+})
+export class ToiletComponent {
+
+}
