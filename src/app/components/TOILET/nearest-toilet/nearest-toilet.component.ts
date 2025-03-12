2be7d627469fdba3309e3f2d0c33e9be13ec6f53
lkas96 <kwek_lawson@hotmail.com>
2025-03-12 17:49:36 +0800

    Placeholder

---
+++ src/app/components/TOILET/nearest-toilet/nearest-toilet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nearest-toilet',
+  standalone: false,
+  templateUrl: './nearest-toilet.component.html',
+  styleUrl: './nearest-toilet.component.css'
+})
+export class NearestToiletComponent {
+
+}
