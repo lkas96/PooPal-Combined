2bc6f4e19e4f61d6e9803192aaaa4dd6d3a4e631
lkas96 <kwek_lawson@hotmail.com>
2025-03-25 00:29:24 +0800

    Updated spring config redis

--- src/main/java/com/lask/poopal_server/poopal_server/configs/JedisConfig.java
+++ src/main/java/com/lask/poopal_server/poopal_server/configs/JedisConfig.java
@@ -1,7 +1,12 @@
 package com.lask.poopal_server.poopal_server.configs;
 
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -10,9 +15,35 @@ import com.lask.poopal_server.poopal_server.models.ChatMessage;
 
 @Configuration
 public class JedisConfig {
-        @Bean
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private Integer redisPort;
+
+    @Value("${spring.data.redis.username}")
+    private String redisUsername;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
+    @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration rsc = new RedisStandaloneConfiguration();
+        rsc.setHostName(redisHost);
+        rsc.setPort(redisPort);
+
+        if(redisUsername.trim().length() > 0) {
+            rsc.setUsername(redisUsername);
+            rsc.setPassword(redisPassword);
+        }
+
+        JedisClientConfiguration jcc = JedisClientConfiguration.builder().build();
+        JedisConnectionFactory jcf = new JedisConnectionFactory(rsc, jcc);
+        jcf.afterPropertiesSet();
+
+        return jcf;
     }
 
     @Bean
